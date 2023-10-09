68f0ed018e9dd94005110db92223b7dfbc2fe9ae
Deanisgarayev <garaevdenis2019@gmail.com>
2023-10-10 00:05:47 +0500

    in progress

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -4,7 +4,6 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import liquibase.pro.packaged.L;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,7 +43,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
             if (update.message().text().equals("/start")) {
                 SendMessage message = new SendMessage(update.message().chat().id(), "Hi");
                 telegramBot.execute(message);
@@ -54,26 +52,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-//        public int extraProcess(List<Update> updates) {
-//        updates.forEach(update -> {
-//            logger.info("Processing update: {}", update);
-//            Pattern dateAndTime = Pattern.compile("[0-9\.\:\s]{16})");
-//            Pattern note = Pattern.compile("(\s)([\W+]+");
-//            Matcher matcherTime = dateAndTime.matcher(update.message().text());
-//            Matcher matcherNote = note.matcher(update.message().text());
-//                // Process your updates here
-//            if (matcherTime.matches() && matcherNote.matches()) {
-//                String time = matcherTime.group(1);
-//                LocalDateTime times = LocalDateTime.parse(time,
-//                        DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-//                String notes = matcherNote.group(2);
-//                NotificationTask notificationTask = new NotificationTask(update.message()
-//                        .messageId(), notes, times);
-//                notificationTaskRepository.save(notificationTask);
-//            }
-//        });
-//        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-//    }
     public List<NotificationTask>notifications(LocalDateTime time) {
         List<NotificationTask> notificationTaskList = notificationTaskRepository.findAll();
         notificationTaskRepository.findAll().forEach(times->{
@@ -84,7 +62,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         return notificationTaskList;
     }
     @Scheduled(cron = "0 0/1 * * * *")
-//    @Scheduled(cron = "0 12 12 *1 MON")
     public void scheduleNotification() {
         List<NotificationTask>notificationTaskList=this.notifications(LocalDateTime.now()
                 .truncatedTo(ChronoUnit.MINUTES));
@@ -98,13 +75,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         });
     }
 
-
     public int extraProcess(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             Pattern pattern = Pattern.compile("([\\d.:\\s]{16})\\s+(.+)");
             Matcher matcher = pattern.matcher(update.message().text());
-            // Process your updates here
             if (matcher.matches()) {
                 String time = matcher.group(1);
                 LocalDateTime times = LocalDateTime.parse(time,
--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -3,10 +3,6 @@ package pro.sky.telegrambot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.telegrambot.model.NotificationTask;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask,Long> {
 
 }
